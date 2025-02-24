3bfc0a5fc54b5696db48708ea633d5ccf06df4d1
skyturtle <dev@skyturtle.io>
2025-02-23 14:00:53 -1000

    add prettier for typescript files

--- lua/plugins/customize.lua
+++ lua/plugins/customize.lua
@@ -96,6 +96,7 @@ return {
         -- Conform will run the first available formatter
         javascript = { "prettierd", "prettier", stop_after_first = true },
         javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+        typescript = { "prettierd", "prettier", stop_after_first = true },
         typescriptreact = { "prettierd", "prettier", stop_after_first = true },
         svelte = { "prettierd", "prettier", stop_after_first = true },
       },
