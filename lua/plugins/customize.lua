5e220a884ba23106c5e5908270019cf5b445dbad
Leo Mendoza <dev@skyturtle.io>
2025-03-13 22:29:40 -0700

    make mini.files mine

--- lua/plugins/customize.lua
+++ lua/plugins/customize.lua
@@ -139,6 +139,21 @@ return {
   },
   {
     "echasnovski/mini.files",
+    opts = {
+      windows = {
+        preview = false,
+        max_number = 3,
+      },
+      mappings = {
+        go_in_plus = "<CR>",
+      },
+      options = {
+        -- Whether to use for editing directories
+        -- Disabled by default in LazyVim because neo-tree is used for that
+        -- I prefer `mini.files` so I'm enabling it
+        use_as_default_explorer = true,
+      },
+    },
     keys = {
       {
         "<leader>fM",
