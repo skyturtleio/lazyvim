f73d8f9fda267737e6f0e8a85512878177bb41d8
Leo Mendoza <dev@skyturtle.io>
2025-03-17 13:56:28 -0700

    update packages

--- lua/plugins/customize.lua
+++ lua/plugins/customize.lua
@@ -1,5 +1,9 @@
 return {
   {
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = false,
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
@@ -137,10 +141,6 @@ return {
         },
       },
     },
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    enabled = false,
   },
   {
     "ibhagwan/fzf-lua",
