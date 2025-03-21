3414ed261e740239f7076fd40c3c4330d7193646
Leo Mendoza <dev@skyturtle.io>
2025-03-20 22:02:50 -0700

    break mini.files into own file

---
+++ lua/plugins/minifiles.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "echasnovski/mini.files",
+    opts = {
+      windows = {
+        max_number = 2,
+        preview = true,
+        width_preview = 60,
+      },
+      mappings = {
+        go_in_plus = "<CR>",
+      },
+      options = {
+        -- Whether to use for editing directories
+        -- Disabled by default in LazyVim because neo-tree is used for that
+        -- I prefer `mini.files` so I'm enabling it
+        use_as_default_explorer = true,
+      },
+    },
+    keys = {
+      {
+        "<leader>fM",
+        function()
+          require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+        end,
+        desc = "Open mini.files (Directory of Current File)",
+      },
+      {
+        "<leader>fm",
+        function()
+          require("mini.files").open(vim.uv.cwd(), true)
+        end,
+        desc = "Open mini.files (cwd)",
+      },
+      { "<leader>e", "<leader>fm", desc = "Open mini.files (Root Dir)", remap = true },
+      { "<leader>E", "<leader>fM", desc = "Open mini.files (cwd)", remap = true },
+    },
+  },
+}
