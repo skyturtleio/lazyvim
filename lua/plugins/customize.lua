fdda6264b2673b7a3f23b85d7c8f81d756427f83
Leo Mendoza <dev@skyturtle.io>
2025-03-31 22:42:56 -0700

    move conform to separate file

--- lua/plugins/customize.lua
+++ lua/plugins/customize.lua
@@ -128,21 +128,6 @@ return {
     },
   },
   {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        elixir = { "mix" },
-        -- Conform will run the first available formatter
-        javascript = { "prettierd", "prettier", stop_after_first = true },
-        javascriptreact = { "prettierd", "prettier", stop_after_first = true },
-        typescript = { "prettierd", "prettier", stop_after_first = true },
-        typescriptreact = { "prettierd", "prettier", stop_after_first = true },
-        svelte = { "prettierd", "prettier", stop_after_first = true },
-        html = { "prettierd", "prettier", stop_after_first = true },
-      },
-    },
-  },
-  {
     "mfussenegger/nvim-lint",
     opts = {
       linters_by_ft = {
