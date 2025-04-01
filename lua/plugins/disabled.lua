822b3e89a7982db08bf03ac9ac1554dc7aef625a
Leo Mendoza <dev@skyturtle.io>
2025-04-01 13:24:04 -0700

    breakout customization to separate files

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    -- I'm using `mini.files`
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = false,
+  },
+  {
+    -- This is what powers LazyVim's fancy-looking tabs
+    -- I didn't like the screen real estate it took up
+    { "akinsho/bufferline.nvim", enabled = false },
+  },
+}
---
+++ lua/plugins/nvim-lint.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        javascript = { "eslint_d" },
+        javascriptreact = { "eslint_d" },
+        typescript = { "eslint_d" },
+        typescriptreact = { "eslint_d" },
+      },
+      linters = {
+        eslint_d = {
+          -- disable `eslint_d` if there is not an `eslint.config.js` file in the project
+          -- directory. It stops the annoying error "Could not parse linter due to...".
+          -- I followed the example from LazyVim's docs for `nvim-lint`
+          -- https://www.lazyvim.org/plugins/linting#nvim-lint
+          condition = function(ctx)
+            return vim.fs.find({ "eslint.config.js" }, { path = ctx.filename, upward = true })[1]
+          end,
+        },
+      },
+    },
+  },
+}
