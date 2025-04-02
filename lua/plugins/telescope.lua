cfd7a38162f1816776c1de9de77d5d6e884f3eba
Leo Mendoza <dev@skyturtle.io>
2025-04-01 22:37:09 -0700

    use telescope ivy theme for files

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      pickers = {
+        find_files = {
+          theme = "ivy",
+        },
+        git_files = {
+          theme = "ivy",
+        },
+        old_files = {
+          theme = "ivy",
+        },
+      },
+    },
+  },
+}
