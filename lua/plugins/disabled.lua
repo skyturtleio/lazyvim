d85d094afceb47119b1aab76e758138653e405ae
Leo Mendoza <dev@skyturtle.io>
2025-04-01 22:40:18 -0700

    disable noice for command-line

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,12 +1,17 @@
 return {
   {
-    -- I'm using `mini.files`
+    -- NOTE: I'm using `mini.files`
     "nvim-neo-tree/neo-tree.nvim",
     enabled = false,
   },
   {
-    -- This is what powers LazyVim's fancy-looking tabs
+    -- NOTE: This is what powers LazyVim's fancy-looking tabs
     -- I didn't like the screen real estate it took up
     { "akinsho/bufferline.nvim", enabled = false },
+  },
+  {
+    -- NOTE: I didn't like the floating command line popup menu
+    "folke/noice.nvim",
+    enabled = false,
   },
 }
