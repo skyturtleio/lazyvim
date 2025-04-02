e5836729ce12d1c56da839dafa2a7cd288fc843e
Leo Mendoza <dev@skyturtle.io>
2025-04-01 22:40:26 -0700

    customize lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function()
+      local opts = {
+        options = {
+          theme = "auto",
+        },
+        sections = {
+          lualine_c = {
+            -- LazyVim.lualine.root_dir(),
+            -- { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+            -- { LazyVim.lualine.pretty_path() },
+            { "filename", path = 4 },
+          },
+          lualine_x = {
+            { "filetype", colored = true },
+          },
+          lualine_y = {
+            "encoding",
+          },
+          lualine_z = {
+            { "progress", separator = " ", padding = { left = 1, right = 0 } },
+            { "location", padding = { left = 0, right = 1 } },
+          },
+        },
+      }
+
+      return opts
+    end,
+  },
+}
