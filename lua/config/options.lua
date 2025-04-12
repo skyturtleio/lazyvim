47bae1ce57a52bee5d291b883272992e6c964c62
Leo Mendoza <dev@skyturtle.io>
2025-04-11 22:50:01 -0500

    set backupcopy for bun hot reload

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,9 @@ vim.g.snacks_animate = false
 
 -- do not hide any markup
 vim.opt.conceallevel = 0
+
+-- need to enable this for Bun hot reloading and watcher to work
+-- e.g. `bun run --hot --watch src/index.ts`
+--
+-- source: https://github.com/oven-sh/bun/issues/8520#issuecomment-2585256608
+vim.opt.backupcopy = "yes"
