6972234e0e3610e3d8f6c4c7d90697102ec51fd4
Leo Mendoza <dev@skyturtle.io>
2025-05-07 22:16:04 -0700

    add workaround for mason.nvim breaking change

---
+++ lua/plugins/mason-workaround.lua
@@ -0,0 +1,7 @@
+return {
+  -- This is a module to deal with Mason.nvim's breaking change.
+  -- LazyVim does not have a fix yet for the [issue](https://github.com/LazyVim/LazyVim/issues/6039)
+  -- source for fix below: https://github.com/LazyVim/LazyVim/issues/6039#issuecomment-2856227817
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+}
