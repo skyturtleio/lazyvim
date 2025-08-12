10e4f94fed86af34d40a42bf940bae9a2cc2d2b2
Leo Mendoza <dev@skyturtle.io>
2025-08-11 21:37:35 -0700

    make only ghost inline completion text brighter

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,11 +3,13 @@ return {
     "folke/tokyonight.nvim",
     opts = {
       on_highlights = function(hl, c)
-        -- Make completion menu text much brighter
-        hl.CmpItemMenu = { fg = c.fg }
-        hl.CmpItemMenuDefault = { fg = c.fg }
+        -- Make completion menu text brighter
         hl.CmpGhostText = { fg = c.fg_dark, italic = true }
-        hl.Comment = { fg = c.fg_gutter, italic = true }
+        -- hl.CmpItemMenu = { fg = c.fg_dark }
+        -- hl.CmpItemMenuDefault = { fg = c.fg_dark }
+        -- hl.Comment = { fg = c.fg_dark, italic = true }
+        -- Make unused/unnecessary code brighter
+        -- hl.DiagnosticUnnecessary = { fg = c.fg_gutter }
       end,
     },
   },
