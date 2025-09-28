7c1c884a5db5292a873dcdca9129b1ef2628f35f
Leo Mendoza <dev@skyturtle.io>
2025-09-28 00:57:28 -0400

    don't italicize comments

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,24 +3,10 @@ return {
     "folke/tokyonight.nvim",
     opts = {
       on_highlights = function(hl, c)
-        -- Make completion menu text brighter
-        hl.CmpGhostText = { fg = c.fg_dark, italic = true }
-        -- hl.CmpItemMenu = { fg = c.fg_dark }
-        -- hl.CmpItemMenuDefault = { fg = c.fg_dark }
-        -- hl.Comment = { fg = c.fg_dark, italic = true }
-        -- Make unused/unnecessary code brighter
-        -- hl.DiagnosticUnnecessary = { fg = c.fg_gutter }
+        hl.CmpGhostText = { fg = c.fg_dark, italic = true } -- Make completion menu text brighter
+        hl.Comment = { fg = c.comment, italic = false } -- Don't italicize comments
       end,
     },
-  },
-  {
-    "navarasu/onedark.nvim",
-    -- priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require("onedark").setup({
-        style = "dark",
-      })
-    end,
   },
   {
     -- Needs to be setup here rather than in `opts`
@@ -28,7 +14,6 @@ return {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "tokyonight-night",
-      -- colorscheme = "onedark",
     },
   },
 }
