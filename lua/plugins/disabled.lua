4d454c2e14012096a5f59f0dcc2af21055b2c29f
Leo Mendoza <dev@skyturtle.io>
2025-10-15 15:01:36 -0400

    use snacks defaults

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -14,11 +14,11 @@ return {
     "folke/noice.nvim",
     enabled = false,
   },
-  {
-    -- NOTE: I don't like the opening dashboard
-    "folke/snacks.nvim",
-    opts = {
-      dashboard = { enabled = false },
-    },
-  },
+  -- {
+  --   -- NOTE: I don't like the opening dashboard
+  --   "folke/snacks.nvim",
+  --   opts = {
+  --     dashboard = { enabled = false },
+  --   },
+  -- },
 }
