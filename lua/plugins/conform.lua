0c2ff291b692015e87c47d8dd85b8db46cdaf154
Leo Mendoza <dev@skyturtle.io>
2025-11-22 13:42:34 -0800

    add comment for elixir formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,6 +3,7 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
+        -- For elixir, let LSP handle formatting
         -- elixir = { "mix" },
         -- heex = { "mix" },
         go = { "gofmt" },
